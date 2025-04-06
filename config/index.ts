2113a38cb5204dee7a72102ad9df0869d814d1a9
GangaLakshmi2002 <gangalakshmi.mudda2002@gmail.com>
2025-04-06 11:56:37 +0530

    mongodb atlas added

--- config/index.ts
+++ config/index.ts
@@ -1,3 +1,5 @@
-export const MONGO_URI ="mongodb://localhost:27017/e-shop";
+
+// export const MONGO_URI ="mongodb://localhost:27017/e-shop";
 // mongodb://localhost:27017/
 export const PORT = process.env.PORT || 9000;
+export const MONGO_URI="mongodb+srv://ganga:ganga333@mern-apps.755m5.mongodb.net/eshop-mobile?retryWrites=true&w=majority&appName=mern-apps";
